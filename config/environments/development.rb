aeb4db39680b09e82f1a4a9a95eefdfb4cb0dcb0
Kelly McLean <kelly.coleman@gmail.com>
2016-12-11 20:02:48 -0800

    Sunday commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,15 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  
-  Paperclip.options[:command_path] = "/usr/local/bin/"
-  
   # config.action_view.raise_on_missing_translations = true
+  Paperclip.options[:command_path] = "/usr/local/bin/convert"
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => "skillcrush-rails",
+          :access_key_id => "AKIAI4Y7CIUPPOFAYN2Q",
+          :secret_access_key => "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"
+      }
+  }
+  
 end
