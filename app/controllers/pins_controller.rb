7e139e68a60ca18f423a0e44cc608c96f425af8d
Kelly McLean <kelly.coleman@gmail.com>
2016-12-19 22:15:34 -0800

    Le sigh Monday

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,5 @@
 class PinsController < ApplicationController
     
-    before_action :require_login, except: [:show, :show_by_name]
-    
     def index
         @pins = Pin.all
         #@pins = current_user.pins
@@ -9,6 +7,11 @@ class PinsController < ApplicationController
     
     def new
         @pin = Pin.new
+    end
+    
+    def show_by_name
+        @pin = Pin.find_by_slug(params[:slug])
+        render :show
     end
     
     def create
@@ -36,24 +39,6 @@ class PinsController < ApplicationController
             @errors = @pin.errors
             render :edit
         end
-    end
-    
-    def show
-        @pin = Pin.find(params[:id])
-        # populate @users with all users who have pinned this pin
-        @users = User.joins(:pinnings).where("users.id = ? or pinnings.pin_id = ?", @pin.user_id, @pin.id).distinct
-        @pins = current_user.pins
-    end
-    
-    def show_by_name
-        @pin = Pin.find_by_slug(params[:slug])
-        render :show
-    end
-    
-    def repin
-        @pin = Pin.find(params[:id])
-        @pin.pinnings.create(user: current_user)
-        redirect_to user_path(current_user)
     end
     
     private
