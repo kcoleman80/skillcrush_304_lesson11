2cd1bf326b6ddb5766898122c5f9a4b1536c767f
Kelly McLean <kelly.coleman@gmail.com>
2016-12-19 21:19:25 -0800

    Monday wtf commit

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,48 +1,65 @@
 class PinsController < ApplicationController
     
-  def index
-    @pins = Pin.all
-  end
-  
-  def show_by_name
-      @pin = Pin.find_by_slug(params[:slug])
-      render :show
-  end
-  
-  def new
-      @pin = Pin.new
-  end
-  
-  def create
-      @pin = Pin.create(params[:pin])
-      if @pin.valid?
-          @pin.save
-          redirect_to pin_path(@pin)
-      else
-      @errors = @pin.errors
-      render :new
-      end
-  end
-  
-  def edit
-      @pin = Pin.find(params[:id])
-      render :edit
-  end
-  
-  def update
-      @pin = Pin.update(params[:id], pin_params)
-      if @pin.valid?
-          @pin.save
-          redirect_to pin_path(@pin)
-          else
-          @errors = @pin.errors
-          render :edit
-      end
-  end
-  
-  private
-  
-  def pin_params
-      params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
-  end
+    before_action :require_login, except: [:show, :show_by_name]
+    
+    def index
+        @pins = Pin.all
+        #@pins = current_user.pins
+    end
+    
+    def new
+        @pin = Pin.new
+    end
+    
+    def create
+        @pin = Pin.create(pin_params)
+        if @pin.valid?
+            @pin.save
+            redirect_to pin_path(@pin)
+            else
+            @errors = @pin.errors
+            render :new
+        end
+    end
+    
+    def edit
+        @pin = Pin.find(params[:id])
+        render :edit
+    end
+    
+    def update
+        @pin = Pin.update(params[:id], pin_params)
+        if @pin.valid?
+            @pin.save
+            redirect_to pin_path(@pin)
+            else
+            @errors = @pin.errors
+            render :edit
+        end
+    end
+    
+    def show
+        @pin = Pin.find(params[:id])
+        # populate @users with all users who have pinned this pin
+        @users = User.joins(:pinnings).where("users.id = ? or pinnings.pin_id = ?", @pin.user_id, @pin.id).distinct
+        @pins = current_user.pins
+    end
+    
+    def show_by_name
+        @pin = Pin.find_by_slug(params[:slug])
+        render :show
+    end
+    
+    def repin
+        @pin = Pin.find(params[:id])
+        @pin.pinnings.create(user: current_user)
+        redirect_to user_path(current_user)
+    end
+    
+    private
+    
+    def pin_params
+        params.require(:pin).permit(:title, :url, :slug, :text, :category_id, :image, :user_id)
+    end
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219063639) do
+ActiveRecord::Schema.define(version: 20161220043439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birthdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pins", force: :cascade do |t|
