ab9213415cee61ff4255c9ce0b2bb41a63610281
Kelly McLean <kelly.coleman@gmail.com>
2016-12-19 19:38:02 -0800

    deploying to heroku

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    #pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "User authenticate method" do
+      before(:all) do
+      @user = User.create(email: "coder@skillcrush", password: "password")
+  end
+  
+  after(:all) do
+      if !@user.destroyed?
+          @user.destroy
+      end
+  end
+  
+  it 'authenticates and returns a user when valid email and password passed in' do
+      @user.authenticate(@user.password)
+      expect(@user).to eq(@user)
+  end
 end
